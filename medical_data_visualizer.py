e2b1d44ed8d7f4fdb799d645d82b56861381b842
Joaquim Schuler <qschuler@uol.com.br>
2024-05-14 19:05:33 +0000

    changed medical_data_visualizer.py, as it was the goal of the project, rewrote README.md, and changed the color theme to light

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv', sep=',', index_col='id')
 
 # 2
-df['overweight'] = None
+bmi = df.weight / (df.height / 100) ** 2
+df['overweight'] = (bmi > 25).astype('int8')
 
 # 3
+for col in ['cholesterol', 'gluc']:
+    df[col] = (df[col] > 1).astype('int8')
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = df.melt(id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.value_counts().reset_index().rename(columns={0: 'total'})
+    df_cat['value'] = df_cat.value.astype('str')  # to match the example figure
 
     # 7
 
-
-
     # 8
-    fig = None
-
+    fig = sns.catplot(data=df_cat, kind='bar', x='variable',
+                      y='total', hue='value', col='cardio',
+                      errorbar=None, height=4, aspect=4 / 3)
 
     # 9
     fig.savefig('catplot.png')
@@ -38,22 +40,25 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    conditions = [(df.height >= df.height.quantile(0.025)),
+                  (df.height <= df.height.quantile(0.975)),
+                  (df.weight >= df.weight.quantile(0.025)),
+                  (df.weight <= df.weight.quantile(0.975))]
+    bool_mask = conditions[0] & conditions[1] & conditions[2] & conditions[3]
+
+    df_heat = df.loc[bool_mask]
 
     # 12
-    corr = None
+    corr = df_heat.corr().round(1)
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr)).astype('bool')
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(layout='tight')
 
     # 15
-
-
+    sns.heatmap(corr, annot=True, linewidth=0.5, mask=mask, cmap='coolwarm', center=0, ax=ax)
 
     # 16
     fig.savefig('heatmap.png')
